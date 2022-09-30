859c3b08f44bde38976a91c5470b600b84baa255
arthurkazaryan <arthur.kazaryan@yahoo.com>
2022-09-30 19:14:48 +0300

    Создана камера на основе RTSP потока, которая закцикливает видео проезжающей мимо машины. Собран docker-compose.yml для запуска камеры.

---
+++ camera/ocr/main.py
@@ -0,0 +1,14 @@
+import easyocr
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time
+image_path = 'plate7.png'
+
+reader = easyocr.Reader(['en'], gpu=True)
+cur_time = time()
+print('start predict')
+result = reader.readtext(image_path)
+
+print(result)
+print(time() - cur_time)
