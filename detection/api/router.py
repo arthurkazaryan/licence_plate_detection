696d282656bd46e34c6ee1eb3ee0228b2f32ffc8
Anastasiya Tataurova <nastia.tataurova@gmail.com>
2022-10-04 23:04:19 +0300

    init dev_nastya

--- detection/api/router.py
+++ detection/api/router.py
@@ -3,6 +3,8 @@ from api.models import DetectionResult
 from fastapi.responses import JSONResponse
 from datetime import datetime
 from detect import YoloV7Model
+from color_detect import ColorModel
+# from type_detect import VehicleTypeModel
 from ocr.main import get_plate_number
 import configparser
 import tempfile
@@ -10,12 +12,15 @@ import easyocr
 import shutil
 import os
 
+
 detection_v1 = APIRouter()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 yolo_v7_model = YoloV7Model.create_model(config_object=config)
 ocr_reader = easyocr.Reader(['en'], gpu=True)
+color_model = ColorModel.create_model(config_object=config)
+# v_type_model = VehicleTypeModel.create_model(config_object=config)
 
 
 @detection_v1.post('/push', tags=['detection'])
@@ -30,13 +35,32 @@ async def post_detecetion(image: UploadFile = File(...)):
     print('COORDINATES', coordinates)
     print('PLATE NUMBER', plate_number)
     os.remove(temp_path)
+        
+    veh_coordinates = [coor_i for coor_i in coordinates['car']
+                               if coor_i[0] < coordinates['vehicle_registration_plate'][0][0] and
+                                  coor_i[1] < coordinates['vehicle_registration_plate'][0][1] and
+                                  coor_i[2] > coordinates['vehicle_registration_plate'][0][2] and
+                                  coor_i[3] > coordinates['vehicle_registration_plate'][0][3]]
+
+    print('VEHICLE COORDINATES', veh_coordinates)
+    if veh_coordinates != []:
+
+        vehicle_color, veh_img = color_model.predict(temp_path, veh_coordinates[0])
+        print('COLOR', vehicle_color)
+        print(veh_img)
+
+        # vehicle_type = '' # v_type_model.predict(veh_img)
+    else:
+        print('Not correct')
+        vehicle_color = 'Error'
+        vehicle_type = 'Error'
 
     return JSONResponse(
         content=DetectionResult(
             camera_id=1,
             date=datetime.now().isoformat(),
-            vehicle_type='cargo',
-            color='white',
+            vehicle_type=vehicle_type,
+            color=vehicle_color,
             number=plate_number.upper() if isinstance(plate_number, str) else 'ERROR!!!'
         ).dict(),
         status_code=status.HTTP_202_ACCEPTED
---
+++ detection/color_detect.py
@@ -0,0 +1,54 @@
+from utils.datasets import LoadImages
+from utils.torch_utils import select_device
+from utils.general import check_img_size
+from ast import literal_eval
+import numpy as np
+import tensorflow as tf
+
+
+class ColorModel:
+
+    def __init__(self, model_path: str, device_type: str, img_size: int,):
+        self.model, self.device = self.load_model(
+            model_path=model_path,
+            device_type=device_type,
+            img_size=img_size
+        )
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.img_size = img_size
+
+    @staticmethod
+    def load_model(model_path: str, device_type: str, no_trace: bool, img_size: int):
+        device = select_device(device_type)
+        half = device.type != 'cpu'
+
+        model = tf.keras.models.load_model(model_path)
+
+        if half:
+            model.half()  # to FP16
+        print('Model loaded.')
+
+        return model, device
+
+    def predict(self, image_path, veh_coordinates):
+
+        img = [] # load and resize image
+        imgsz = check_img_size(self.img_size)
+        img = LoadImages(image_path, img_size=imgsz)
+
+        id_color = {0:'black', 1:'blue', 2:'cyan', 3:'gray', 4:'green', 5:'red', 6:'white', 7:'yellow'}
+        pred_color = id_color[np.argmax(model.predict(img))]
+
+        return pred_color, img
+
+    @classmethod
+    def create_model(cls, config_object):
+        model_path = config_object['ColorModel']['weights']
+        device_type = config_object['ColorModel']['device_type']
+        img_size = literal_eval(config_object['ColorModel']['img_size'])
+
+        return cls(
+            model_path=model_path,
+            device_type=device_type,
+            img_size=img_size,
+        )
